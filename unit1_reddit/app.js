1351cb70ebc1ebcbcd5332fa7d2d303749b59a76
sirinapha88 <sirinapha888@gmail.com>
2016-01-26 17:33:56 -0800

    made change reddit clone

--- unit1_reddit/app.js
+++ unit1_reddit/app.js
@@ -9,6 +9,10 @@ app.controller("redditController", function($scope){
   	$scope.newPost = {};
   	$scope.view = {};
   	$scope.votes = 0;
+  	$scope.view.search = "";
+  	$scope.commentsVisible = false;
+  	$scope.view.sortableFields = ['votes', 'date', 'title'];
+  	$scope.view.currentSort = $scope.view.sortableFields[0];
 
   	$scope.view.posts = [
   		{
@@ -20,23 +24,34 @@ app.controller("redditController", function($scope){
 	  		date: mm+'/'+dd+'/'+yyyy,
 	  		comments: [
 	        {
-	          author: "Matt",
-	          text: "Cool costume."
+	          	author: "Matt",
+	          	text: "Cool costume."
 	        }
 	      	]
   		},{
-  			title: "Teach something",
-	  		author: "Siri",
-	  		desc: "If you're passionate about a particular subject, and you have a lot of experience in that area",
-	  		imgURL: "http://barkpost.com/wp-content/uploads/2015/10/reddit-dog-jokes-20-808x808.jpg",
-	  		votes: 2,
+  			title: "Spy on your competition",
+	  		author: "Ana",
+	  		desc: "Take a look at other blogs on your topic to see what other people are already doing.",
+	  		imgURL: "http://barkpost.com/wp-content/uploads/2015/10/gIzeTnf-808x808.jpg",
+	  		votes: 5,
 	  		date: mm+'/'+dd+'/'+yyyy,
 	  		comments: [
 	        {
-	          author: "Matt",
-	          text: "Cool costume."
-	        }
-	      	]
+	          	author: "Matt",
+          		text: "Sound Great!"
+	        }]
+	    },{
+  			title: "Teach something",
+	  		author: "Siri",
+	  		desc: "If you're passionate about a particular subject, and you have a lot of experience in that area",
+	  		imgURL: "http://barkpost.com/wp-content/uploads/2015/10/reddit-jokes-3-808x808.jpg",
+	  		votes: 8,
+	  		date: mm+'/'+dd+'/'+yyyy,
+	  		comments: [
+	        {
+	          	author: "David",
+	          	text: "Cool!"
+	        }]
 	    }
   	];
 	
@@ -60,12 +75,17 @@ app.controller("redditController", function($scope){
   	$scope.addComment = function(post,newComment){
   		post.comments.push(newComment);
   		$scope.newComment = {};
+  		$scope.commentsVisible = true;
   	};
 	
 
-  	$scope.updateVotes = function(changeVal) {
-    	$scope.view.posts.votes += changeVal;
+  	$scope.updateVotes = function(post, changeVal) {
+    	 post.votes += changeVal;
   	};
+
+	// $scope.showComment = function(){
+
+	// };
 
 });
 
